a6118a437806f0e6f95c105cc837c9bbcd72b946
Micah Usher <mrusher@gmail.com>
2021-02-17 03:24:29 +0000

    tried adding npm install in shell to see if github will grade it now.

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,7 +18,7 @@ let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-} candidateName = input.question("what is your name? \t")
+} candidateName = input.question("What is your name? \t")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
