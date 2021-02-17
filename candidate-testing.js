6e95d2fcef7baeb8eb52d5a7596ba9b2fc9acaf1
Micah Usher <mrusher@gmail.com>
2021-02-17 18:01:55 +0000

    Troubleshooted the spacing, wording and line breaks, got autgrader to work on replit and the code is passing it on there.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,8 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? \t", 
-"True or False: 5000 Meters = 5 Kilometers?\t", 
-"(5+3)/2*10 = ?\t", 
-"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?]\t", "What is the minimum crew size for the ISS?\t"];
-let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+let correctAnswers = [ 'Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswers = [];
 
 
